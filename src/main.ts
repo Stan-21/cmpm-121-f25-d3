0749a22a1d15bb472ba6b9caf23d88fd18ec9abf
Stan-21 <77824280+Stan-21@users.noreply.github.com>
2025-11-09 08:38:10 +0000

    minor code cleanup + standardizing messages between clicks

--- src/main.ts
+++ src/main.ts
@@ -21,7 +21,7 @@ const mapDiv = document.createElement("div");
 mapDiv.id = "map";
 document.body.append(mapDiv);
 
-let heldToken: number | null = 5;
+let heldToken: number | null = 1;
 
 const statusPanelDiv = document.createElement("div");
 statusPanelDiv.id = "statusPanel";
@@ -70,14 +70,13 @@ function spawnCache(x: number, y: number) {
   const rect = leaflet.rectangle(bounds).addTo(map);
 
   let rectPoints: number | null = Math.ceil(
-    luck([x, y, "initialValue"].toString()) * 8,
+    luck([x, y, "initialValue"].toString()) * 4,
   );
   const popupDiv = document.createElement("div");
   popupDiv.innerHTML =
     `<div><span id="message">There is a cache at ${x},${y}.  It has a token of ${rectPoints}.</span></div>
 <button id="poke">poke</button><button id="craft">craft</button><button id = "store">store</button>`;
 
-  // Clicking the button decrements the cache's value and increments the player's points
   popupDiv.querySelector<HTMLButtonElement>("#poke")!.addEventListener(
     "click",
     () => {
@@ -87,8 +86,9 @@ function spawnCache(x: number, y: number) {
         statusPanelDiv.innerHTML = `${heldToken}`;
         rectPoints = null;
         popupDiv.querySelector<HTMLSpanElement>("#message")!.innerHTML =
-          "There is a cache here.  Currently there is no token.";
+          `There is a cache at ${x},${y}.  Currently there is no token.`;
         popupDiv.querySelector<HTMLButtonElement>("#poke")!.disabled = true;
+        popupDiv.querySelector<HTMLButtonElement>("#store")!.disabled = false;
       } else if (rectPoints) {
         console.log(
           `You have a token in your inventory.  Swapping inventory with cache`,
@@ -98,7 +98,7 @@ function spawnCache(x: number, y: number) {
         rectPoints = temp;
         statusPanelDiv.innerHTML = `${heldToken}`;
         popupDiv.querySelector<HTMLSpanElement>("#message")!.innerHTML =
-          `There is a cache here.  It has a token of ${rectPoints}`;
+          `There is a cache at ${x},${y}.  It has a token of ${rectPoints}`;
       } else {
         console.log("There is nothing here that could be poked!");
       }
@@ -116,19 +116,16 @@ function spawnCache(x: number, y: number) {
         );
         rectPoints! *= 2;
         popupDiv.querySelector<HTMLSpanElement>("#message")!.innerHTML =
-          `There is a cache here.  It has a token of ${rectPoints}.`;
+          `There is a cache at ${x},${y}. It has a token of ${rectPoints}.`;
         heldToken = null;
         statusPanelDiv.innerText = `${heldToken}`;
+        popupDiv.querySelector<HTMLButtonElement>("#store")!.disabled = true;
       } else {
         console.log(`Cannot craft!`);
       }
     },
   );
 
-  // OUTCOMES FOR STORING
-  // Player has token, cache has no token: Store token into cache, remove from player
-  // Cache has token, player has no token: Cannot store, player has no token
-  // Both have a token: SWAP
   popupDiv.querySelector<HTMLButtonElement>("#store")!.addEventListener(
     "click",
     () => {
@@ -141,14 +138,14 @@ function spawnCache(x: number, y: number) {
         rectPoints = temp;
         statusPanelDiv.innerHTML = `${heldToken}`;
         popupDiv.querySelector<HTMLSpanElement>("#message")!.innerHTML =
-          `There is a cache here.  It has a token of ${rectPoints}`;
+          `There is a cache at ${x},${y}.  It has a token of ${rectPoints}`;
       } else if (heldToken) {
         console.log(`Storing token into cache`);
         rectPoints = heldToken;
         heldToken = null;
         statusPanelDiv.innerHTML = `${heldToken}`;
         popupDiv.querySelector<HTMLSpanElement>("#message")!.innerHTML =
-          `There is a cache here.  It has a token of ${rectPoints}`;
+          `There is a cache at ${x},${y}.  It has a token of ${rectPoints}`;
         popupDiv.querySelector<HTMLButtonElement>("#poke")!.disabled = false;
       } else {
         console.log("Player has no token.  Cannot store anything");
